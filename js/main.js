3402f6e9395fe72362d9b0b425cb828a7538c8f0
Ovy95 <jackoverton21@hotmail.com>
2021-01-25 10:39:25 +0000

    Contact form now sends emails using Formspree

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 console.log("link from main")
 
+$(document).ready(function(){
+  $(".div5button").click(function(event){
+    
+    console.log('Clicked button')
+
+    var email = $(".email").val()
+    var fullname = $(".fullname").val()
+    var message = $(".message").val()
+    var statusElm = $(".status")
+    statusElm.empty()
+
+    if(email.length > 5 && email.includes('@') && email.includes(".")) {
+
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Email is NOT valid</div>')
+    }
+
+    if(fullname.length > 1 ) {
+
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Name is NOT valid</div>')
+    }
+    if(message.length > 1 ) {
+
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Message is NOT valid</div>')
+    }
+
+
+  });
+});
+
 
 $(document).ready(function(){
   $("p").click(function(){
